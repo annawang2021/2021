e7d78c835d41cf1f2dfb8724c883cadfac2efc20
annawang2021 <77376405+annawang2021@users.noreply.github.com>
2021-03-28 17:40:46 +0800

    Update data.sql

--- assignment/week5/data.sql
+++ assignment/week5/data.sql
@@ -1,20 +1,90 @@
-?         (\?) Synonym for `help'.
-clear     (\c) Clear the current input statement.
-connect   (\r) Reconnect to the server. Optional arguments are db and host.
-delimiter (\d) Set statement delimiter.
-ego       (\G) Send command to MariaDB server, display result vertically.
-exit      (\q) Exit mysql. Same as quit.
-go        (\g) Send command to MariaDB server.
-help      (\h) Display this help.
-notee     (\t) Don't write into outfile.
-print     (\p) Print current command.
-prompt    (\R) Change your mysql prompt.
-quit      (\q) Quit mysql.
-rehash    (\#) Rebuild completion hash.
-source    (\.) Execute an SQL script file. Takes a file name as an argument.
-status    (\s) Get status information from the server.
-tee       (\T) Set outfile [to_outfile]. Append everything into given outfile.
-use       (\u) Use another database. Takes database name as argument.
-charset   (\C) Switch to another charset. Might be needed for processing binlog with multi-byte charsets.
-warnings  (\W) Show warnings after every statement.
-nowarning (\w) Don't show warnings after every statement.
+-- MariaDB dump 10.18  Distrib 10.4.17-MariaDB, for Win64 (AMD64)
+--
+-- Host: localhost    Database: website
+-- ------------------------------------------------------
+-- Server version	5.7.18-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `website`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `website` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `website`;
+
+--
+-- Table structure for table `message`
+--
+
+DROP TABLE IF EXISTS `message`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `message` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(20) NOT NULL,
+  `content` varchar(255) NOT NULL,
+  `time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `message_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `message`
+--
+
+LOCK TABLES `message` WRITE;
+/*!40000 ALTER TABLE `message` DISABLE KEYS */;
+INSERT INTO `message` VALUES (10,17,'尺寸太小','2021-03-28 09:17:40'),(11,22,'出貨速度快','2021-03-28 09:18:11'),(12,21,'服務態度好','2021-03-28 09:18:32'),(13,19,'顏色稍有落差','2021-03-28 09:18:56'),(14,13,'出貨速度快','2021-03-28 09:20:17'),(15,11,'顏色稍有落差','2021-03-28 09:21:01');
+/*!40000 ALTER TABLE `message` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (11,'丁滿','ply','ply','2021-03-28 08:42:53'),(12,'拉拉','test01','test01','2021-03-28 08:43:58'),(13,'花花','test02','test02','2021-03-28 08:56:25'),(14,'小江','test04','test04','2021-03-28 08:56:55'),(15,'阿斌','test03','test03','2021-03-28 08:57:24'),(16,'阿姆','test05','test05','2021-03-28 09:12:40'),(17,'粉豬','test06','test06','2021-03-28 09:13:30'),(18,'黑貓','test07','test07','2021-03-28 09:13:46'),(19,'灰狼','test08','test08','2021-03-28 09:14:02'),(20,'神風','test09','test09','2021-03-28 09:14:17'),(21,'神風','test10','test10','2021-03-28 09:14:44'),(22,'鰲拜','test11','test11','2021-03-28 09:15:00'),(23,'三叔','test12','test12','2021-03-28 09:15:17'),(24,'小寶','test13','test13','2021-03-28 09:15:41');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-03-28 16:41:47
