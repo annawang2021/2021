4ffefe3e4002f8090cea35e56a11d0bf3538d767
annawang2021 <77376405+annawang2021@users.noreply.github.com>
2021-04-05 10:11:32 +0800

    Update week6.py

--- assignment/week6/week6.py
+++ assignment/week6/week6.py
@@ -8,7 +8,7 @@ app.secret_key = b'\xc1@\xbbnla\x8a8\x97\xc4\x97e(K\xea\n'
 mydb = mysql.connector.connect(
   host="localhost",
   user="root",
-  password="root",
+  password="",
   database="membership"
 )
 
